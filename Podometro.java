c06bf93486d5efcf607067496ab4ed348c23d120
Christhoper Pinday  Delgado <cpindayddam1@gmail.com>
2021-10-14 10:48:20 +0200

    Constantes y atributos declarados en el cuerpo de la clase

--- Podometro.java
+++ Podometro.java
@@ -7,8 +7,32 @@
  */
 
 public class Podometro {
+     //constantes que indican el sexo de una persona
+    private final char HOMBRE = 'H';
+    private final char MUJER = 'M';
+    //Constantes, representan un porcentaje (45% y 41%) sobre la altura de 
+    //de la persona que se usaran para calcular la long. de su zancada
+    private final double ZANCADA_HOMBRE = 0.45;
+    private final double ZANCADA_MUJER = 0.41;
+    //Dos constantes que indican el nº de dia de la semana
+    private final int SABADO = 6;
+    private final int DOMINGO = 7; 
     
-
+    //ATRIBUTOS
+    private String marca; //Marca del podómetro
+    //A. de Configuración
+    private double altura; //altura de la persona (cm)
+    private char sexo; //sexo de la persona 'M' o 'H'
+    private double longitudZancada; //Almacena la long. de la zancada de la persona en cm
+    // A. de Estadísticas
+    private int totalPasosLaborables; //nº de pasos dados de lun a vie
+    private int totalPasosSabado; //guarda el nº de pasos dados el sabado
+    private int totalPasosDomingo; //nº pasos del domingo
+    private double totalDistanciaSemana; //distancia recorrida en toda la semana (km)
+    private double totalDistanciaFinSemana; //distancia recorrida el fin de semana (km)
+    private int tiempo; //tiempo total caminado en la semana (en min)
+    private int caminatasNoche; //Nro. de caminatas (paseos) dados a partir de las 21h en toda la semana
+    
     /**
      * Inicializa el podómetro con la marca indicada por el parámetro.
      * El resto de atributos se ponen a 0 y el sexo, por defecto, es mujer
